f47e94bb4344e56906996c5421781cb5b315954d
Jaume Parrot Altisent <jaumeparrotaltisent@MacBook-Pro-de-Jaume.local>
2021-08-21 12:39:55 +0200

    Install stripe

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,6 +6,7 @@ const config = {
   secretAccessKey: process.env.AWSSecretKey,
   accessKeyId: process.env.AWSAccessKeyId,
   tokenSecret: process.env.TOKEN_SECRET,
+  stripeSecreyKey: process.env.STRIPE_SECREY_KEY,
 };
 
 export default config;
